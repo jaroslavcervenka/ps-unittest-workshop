f2c09fdd937f6badb4268103cdbeef405bca43cf
Jaroslav Cervenka <jaroslavc@oriflame.com>
2017-12-21 09:18:28 +0100

    Fix rename file command

---
+++ 2. New fixture/Rename-File.ps1
@@ -0,0 +1,12 @@
+﻿function Rename-File {
+    param(
+        [string] $File,
+        [string] $NewName
+    )
+
+    if(!(Test-Path -Path $File)){
+        throw "File nod found in path $File."
+    }
+
+    Rename-Item -Path $File -NewName $NewName
+}
